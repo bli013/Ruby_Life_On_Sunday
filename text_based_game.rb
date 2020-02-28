361b1965e0fb27336eabc9515a905764957a17e6
heyabird <heyabird@gmail.com>
2020-02-27 17:04:02 -0800

    Mostly done but needs loop finished

---
+++ text_based_game.rb
@@ -0,0 +1,325 @@
+
+def get_couch_repeat
+puts "Now you have FOMO, call them?"
+puts "1.Yes or 1.YES or 1.You have no choice... really."
+user_input8 = gets.chomp.to_i
+    if user_input8 == 1
+        puts "Brunch has been great, you have had some great food and some great drinks. Now one of your friends suggest going on a bar crawl they are going to, and want to continue the fun, what do you say?"
+        puts "1.Tik-tock on the clock but the party don't stop! OR 2.No thanks you, I have obligations tomorrow and should go home and rehydrate."
+        user_input9 = gets.chomp.to_i
+            if user_input9 == 1
+                puts "You are now Bar Crawling and it is 7pm. Your house is on the way to the next bar, do you go home?"
+                puts "1.Yes, I am going home and getting hydrated up! OR 2.Don't stop make it pop, DJ blow my speakers up, Tonight imma fight til we see the sunlight!"
+                    user_input10 = gets.chomp.to_i
+                        if user_input10 == 1
+                            puts "It is now Monday morning, you have had better days but you made it to your obligation. Little dazed but Life is Great."
+                        elsif user_input10 == 2
+                            puts "Okay... You lived the life of 2009 Ke$ha and have missed being at your obligations, BUT what a night! Now you will be Hungover ALL day."
+                        end
+            elsif user_input9 == 2
+                puts "It is now Monday morning, you have had better days but you made it to your obligation. Little dazed but Life is Great."
+            end
+    elsif user_input8 == 3
+        puts "Fine, you really don't like your friends, have a nice day..."
+    end
+end
+
+def get_brunch
+    puts "
+    )==(
+        )==(
+        |H |
+        |H |
+        |H |
+       /====.
+      / Dr S .
+     /========.
+    :HHHHHHHH H:
+    |HHHHHHHH H|
+    |HHHHHHHH H|
+    |HHHHHHHH H|
+L______|=/==== L________/
+.     :/oO/    |       /
+.    / oOOO    |      /
+.__/| OOO ALCHY|   __/
+ )( |  O       |   )(
+ )( |==========|   )(
+ )( |HHHHHHHH H|   )(
+ )( |HHHHHHHH H|   )(
+.)(.|HHHHHHHH H|  .)(.
+~~~~~~~~~~~~~~~~  ~~~~~~
+    "
+    puts "Brunch has been great, you have had some great food and some great drinks. Now one of your friends suggest going on a bar crawl they are going to, and want to continue the fun, what do you say?"
+    puts "1.Tik-tock on the clock but the party don't stop! OR 2.No thanks you, I have obligations tomorrow and should go home and rehydrate."
+    user_input20 = gets.chomp.to_i
+        if user_input20 == 1
+            puts "You are now Bar Crawling and it is 7pm. Your house is on the way to the next bar, do you go home?"
+            puts "1.Yes, I am going home and getting hydrated up! OR 2.Don't stop make it pop, DJ blow my speakers up, Tonight imma fight til we see the sunlight!"
+                user_input21 = gets.chomp.to_i
+                    if user_input21 == 1
+                        puts "
+                        [___________]
+                                             {=========}
+                                          ..-'         '-..
+                                         (                 )
+                                         (_________________)
+                                         | _  _  _   _     |
+                                         ||A(_ |_)||_)||A ||
+                        ,.--.   ,.--.    ||~A_)|  || R|| A||
+                       // >  ) // >  )   |_________________|
+                       ((  > / ((  > /   |                 |
+                        `'--'   `'--'    '-----------------'
+                        "
+                        puts "It is now Monday morning, you have had better days but you made it to your obligation. Little dazed but Life is Great."
+                    elsif user_input21 == 2
+                        puts "Okay... You lived the life of 2009 Ke$ha and have missed being at your obligations, BUT what a night! Now you will be Hungover ALL day."
+                    end
+        elsif user_input20 == 2
+            puts "It is now Monday morning, you have had better days but you made it to your obligation. Little dazed but Life is Great."
+        end
+end
+puts ""
+puts ""
+puts "
+_^^^_                 |                _^^^_
+<<o>>             7       7            17o71
+  |                 .---.                |
+ _|_______     --  (     )  --     ______|__
+         `~^~^~^~^~^~^~^~^~^~^~^~`
+Jonas               Richie               Tsz
+         "
+puts ""
+puts ""
+puts ""
+puts "Welcome to Life on Sundays"
+puts "********************************"
+puts "Please full screen the terminal"
+puts "***************************************"
+puts "You wake up in the morning feeling like P.Diddy, do you make the move to get a cup of coffee or tea?"
+puts "1.Yes, Caffeine is Life. OR 2.No, I'll just brush my teeth with a bottle of Jack."
+user_input = gets.chomp.to_i
+    if  user_input == 1
+        puts "
+              )  (
+             (   ) )
+              ) ( (
+            _______)_
+         .-'---------|
+        ( C|/|||||||||
+         '-./|||||||||
+           '_________'
+            '-------'
+
+        "
+
+        puts "So you decided to make the move to get some coffee in you, and now your friends call you and ask if you want to go to brunch, what do you say?"
+            puts "1.No, Gym is Life. OR 2.Yeah, brunch sounds fun with friends."
+                user_input1 = gets.chomp.to_i
+                    if user_input1 == 1
+                        puts "You made it home now, and you see yourself in the mirror, You see the GAINS! but, then you realize your gains are holding your social life back and you missed out on a good time with friends."
+                    elsif user_input1 == 2
+                        puts "
+                            )==(
+                            )==(
+                            |H |
+                            |H |
+                            |H |
+                           /====.
+                          / Dr S .
+                         /========.
+                        :HHHHHHHH H:
+                        |HHHHHHHH H|
+                        |HHHHHHHH H|
+                        |HHHHHHHH H|
+                    L______|=/==== L________/
+                    .     :/oO/    |       /
+                    .    / oOOO    |      /
+                    .__/| OOO ALCHY|   __/
+                     )( |  O       |   )(
+                     )( |==========|   )(
+                     )( |HHHHHHHH H|   )(
+                     )( |HHHHHHHH H|   )(
+                    .)(.|HHHHHHHH H|  .)(.
+                    ~~~~~~~~~~~~~~~~  ~~~~~~
+                        "
+                        puts "Brunch has been great, you have had some great food and some great drinks. Now one of your friends suggest going on a bar crawl they are going to, and want to continue the fun, what do you say?"
+                            puts "1.Tik-tock on the clock but the party don't stop! OR 2.No thanks you, I have obligations tomorrow and should go home and rehydrate."
+                            user_input2 = gets.chomp.to_i
+                                if user_input2 == 1
+                                    puts "You are now Bar Crawling and it is 7pm. Your house is on the way to the next bar, do you go home?"
+                                    puts "1.Yes, I am going home and getting hydrated up! OR 2.Don't stop make it pop, DJ blow my speakers up, Tonight imma fight til we see the sunlight!"
+                                    user_input3 = gets.chomp.to_i
+                                        if user_input3 == 1
+                                            puts "It is now Monday morning, you have had better days but you made it to your obligation. Little dazed but Life is Great."
+                                        elsif user_input3 == 2
+                                            puts "Okay... You lived the life of 2009 Ke$ha and have missed being at your obligations, BUT what a night! Now you will be Hungover ALL day."
+                                        end
+                                elsif user_input2 == 2
+                                    puts "
+                                    [___________]
+                                                         {=========}
+                                                      ..-'         '-..
+                                                     (                 )
+                                                     (_________________)
+                                                     | _  _  _   _     |
+                                                     ||A(_ |_)||_)||A ||
+                                    ,.--.   ,.--.    ||~A_)|  || R|| A||
+                                   // >  ) // >  )   |_________________|
+                                   ((  > / ((  > /   |                 |
+                                    `'--'   `'--'    '-----------------'
+                                    "
+                                    puts "It is now Monday morning, you have had better days but you made it to your obligation. Little dazed but Life is Great."
+                                end
+                    end
+    elsif user_input == 2
+        puts "
+           ( _ _ _)
+            |_ _ |
+           (_ _ _ )
+            |    |
+            |____|
+            |    |
+            (2837)
+            | __ |
+            |    |
+            |    |
+           ( .--. )
+        .-' :(..): `-.
+    _.-'    `-..-'    `-._
+  .'                      `.
+  |___                  ___|
+  |   `````--....--'''''   |
+  |  |`````--....--'''''|  |
+  |  |                  |  |
+  |  |                  |  |
+  |  |                  |  |
+  |  |                  |  |
+  |  |                  |  |
+  |  |                  |  |
+  |  |                  |  |
+  |  |                  |  |
+  |  |                  |  |
+  |  |                  |  |
+  |  |                  |  |
+  |  |                  |  |
+  |  |  _____    _____  |  |
+  |  |   JACK Daniels   |  |
+ .'  |                  |  `.
+(  `-.`````--....--'''''.-'  )
+ ``-.._``--.._.._..:--  :.--''
+       ``--.._  _..--''
+              `"
+
+        puts "You just brushed your teeth with a bottle of Jack, and now your friends call you and ask if you want to go to brunch, what do you say?"
+        puts "1.No, there ain't no coming back. Because I'm gonna go knock out on the couch. OR 2.Yeah, that sounds great!"
+        user_input4 = gets.chomp.to_i
+            if user_input4 == 1
+                puts "Now you have been on the couch, you see your friends on social media, do you call them?"
+                puts "1.Yes, let's call them and see what is happening now. 2.Nah"
+                user_input5 = gets.chomp.to_i
+                    if user_input5 == 1
+                        puts "
+                            )==(
+                            )==(
+                            |H |
+                            |H |
+                            |H |
+                           /====.
+                          / Dr S .
+                         /========.
+                        :HHHHHHHH H:
+                        |HHHHHHHH H|
+                        |HHHHHHHH H|
+                        |HHHHHHHH H|
+                    L______|=/==== L________/
+                    .     :/oO/    |       /
+                    .    / oOOO    |      /
+                    .__/| OOO ALCHY|   __/
+                     )( |  O       |   )(
+                     )( |==========|   )(
+                     )( |HHHHHHHH H|   )(
+                     )( |HHHHHHHH H|   )(
+                    .)(.|HHHHHHHH H|  .)(.
+                    ~~~~~~~~~~~~~~~~  ~~~~~~
+                        "
+                        puts "Brunch has been great, you have had some great food and some great drinks. Now one of your friends suggest going on a bar crawl they are going to, and want to continue the fun, what do you say?"
+                        puts "1.Tik-tock on the clock but the party don't stop! OR 2.No thanks you, I have obligations tomorrow and should go home and rehydrate."
+                        user_input6 = gets.chomp.to_i
+                            if user_input6 == 1
+                                puts "You are now Bar Crawling and it is 7pm. Your house is on the way to the next bar, do you go home?"
+                                puts "1.Yes, I am going home and getting hydrated up! OR 2.Don't stop make it pop, DJ blow my speakers up, Tonight imma fight til we see the sunlight!"
+                                    user_input7 = gets.chomp.to_i
+                                        if user_input7 == 1
+                                            puts "It is now Monday morning, you have had better days but you made it to your obligation. Little dazed but Life is Great."
+                                        elsif user_input7 == 2
+                                            puts "Okay... You lived the life of 2009 Ke$ha and have missed being at your obligations, BUT what a night! Now you will be Hungover ALL day."
+                                        end
+                            elsif user_input6 == 2
+                                puts "It is now Monday morning, you have had better days but you made it to your obligation. Little dazed but Life is Great."
+                            end
+                    end
+            elsif user_input4 == 2
+                get_brunch
+            end
+
+                    elsif user_input5 == 2
+                        puts "Now you have FOMO, call them?"
+                        puts "1.Yes or 2.No"
+                        user_input8 = gets.chomp.to_i
+                            if user_input8 == 1
+                                puts "
+                                    )==(
+                                    )==(
+                                    |H |
+                                    |H |
+                                    |H |
+                                   /====.
+                                  / Dr S .
+                                 /========.
+                                :HHHHHHHH H:
+                                |HHHHHHHH H|
+                                |HHHHHHHH H|
+                                |HHHHHHHH H|
+                            L______|=/==== L________/
+                            .     :/oO/    |       /
+                            .    / oOOO    |      /
+                            .__/| OOO ALCHY|   __/
+                             )( |  O       |   )(
+                             )( |==========|   )(
+                             )( |HHHHHHHH H|   )(
+                             )( |HHHHHHHH H|   )(
+                            .)(.|HHHHHHHH H|  .)(.
+                            ~~~~~~~~~~~~~~~~  ~~~~~~
+                                "
+                                puts "Brunch has been great, you have had some great food and some great drinks. Now one of your friends suggest going on a bar crawl they are going to, and want to continue the fun, what do you say?"
+                                puts "1.Tik-tock on the clock but the party don't stop! OR 2.No thanks you, I have obligations tomorrow and should go home and rehydrate."
+                                user_input9 = gets.chomp.to_i
+                                    if user_input9 == 1
+                                        puts "You are now Bar Crawling and it is 7pm. Your house is on the way to the next bar, do you go home?"
+                                        puts "1.Yes, I am going home and getting hydrated up! OR 2.Don't stop make it pop, DJ blow my speakers up, Tonight imma fight til we see the sunlight!"
+                                            user_input10 = gets.chomp.to_i
+                                                if user_input10 == 1
+                                                    puts "It is now Monday morning, you have had better days but you made it to your obligation. Little dazed but Life is Great."
+                                                elsif user_input10 == 2
+                                                    puts "Okay... You lived the life of 2009 Ke$ha and have missed being at your obligations, BUT what a night! Now you will be Hungover ALL day."
+                                                end
+                                    elsif user_input9 == 2
+                                        puts "
+                                        [___________]
+                                                             {=========}
+                                                          ..-'         '-..
+                                                         (                 )
+                                                         (_________________)
+                                                         | _  _  _   _     |
+                                                         ||A(_ |_)||_)||A ||
+                                        ,.--.   ,.--.    ||~A_)|  || R|| A||
+                                       // >  ) // >  )   |_________________|
+                                       ((  > / ((  > /   |                 |
+                                        `'--'   `'--'    '-----------------'
+                                        "
+                                        puts "It is now Monday morning, you have had better days but you made it to your obligation. Little dazed but Life is Great."
+                                    end
+                            elsif user_input8 == 2
+                                 get_couch_repeat
+                            end
+                    end
